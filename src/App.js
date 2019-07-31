4f636553b649608eb481c45bc6b09e2d84d7c892
unknown <toosdai52Otte@gmail.com>
2019-07-31 15:15:23 -0500

    accessed api information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Title from './components/Title';
+import Img from './components/Img';
+import Explanation from './components/Explanation';
 import "./App.css";
 
 function App() {
+
+  const [img, setImg] = useState();
+  const [title, setTitle] = useState();
+  const [x, setX] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then( result => {
+        console.log(result.data)
+        setImg(result.data.url);
+        setTitle(result.data.title);
+        setX(result.data.explanation);
+
+      }) .catch ((error) => {
+        console.log(error)
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <p>
+      /* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+    </div> */
+    <div className="header">
+      <Img src={img} />
+      <Title title={title} />
+      <Explanation info={x} />
     </div>
   );
 }
---
+++ src/components/Img.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Img(props){
+    if (!props.src) return <h3>Loading...</h3>;
+    return <img className="picture" src={props.src} alt=""/>
+}
